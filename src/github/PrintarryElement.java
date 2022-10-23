fcff0cfe35fd16b6d71a0f35699d567233f13a9f
Archana GN <Archana GN@DESKTOP-8TMDF5I>
2022-10-23 16:37:47 +0530

    third commit

--- src/github/PrintarryElement.java
+++ src/github/PrintarryElement.java
@@ -5,8 +5,10 @@ public class PrintarryElement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[]= {1,3,4,6,7};
-		
-
-	}
+		for(int i=0;i<arr.length;i++)
+		{
+		System.out.println(arr[i]);
+		}
+		}
 
 }
