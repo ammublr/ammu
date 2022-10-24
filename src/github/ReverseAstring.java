775e827765eb6055a90e964db9bef11324d18b82
Archana GN <Archana GN@DESKTOP-8TMDF5I>
2022-10-24 09:15:54 +0530

    committing a nagsha brach

--- src/github/ReverseAstring.java
+++ src/github/ReverseAstring.java
@@ -4,11 +4,11 @@ public class ReverseAstring {
 	public static void main(String[] args) {
 		String s="archananagsha";
 		String rev="";
-		for(int i=0;i<s.length();i++)
-		{
-			rev=s.charAt(i)+rev;
-		}
-		System.out.println(rev);
+//		for(int i=0;i<s.length();i++)
+//		{
+//			rev=s.charAt(i)+rev;
+//		}
+//		System.out.println(rev);
 		
 		
 	}
